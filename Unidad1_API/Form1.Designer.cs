a2937d6be8a031c71818fac09cc6df1849f200b4
JUAN DAVID........ LOPEZ <juanda-1712@hotmail.com>
2022-09-03 14:21:57 -0500

    Agregar archivos de proyecto.

---
+++ Unidad1_API/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Unidad1_API
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pbx = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(102, 53);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(246, 22);
+            this.txtnombre.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Cyan;
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkGray;
+            this.button1.Location = new System.Drawing.Point(29, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(319, 39);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Enviar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pbx
+            // 
+            this.pbx.Location = new System.Drawing.Point(614, 43);
+            this.pbx.Name = "pbx";
+            this.pbx.Size = new System.Drawing.Size(110, 148);
+            this.pbx.TabIndex = 6;
+            this.pbx.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Cyan;
+            this.button2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.DarkGray;
+            this.button2.Location = new System.Drawing.Point(29, 203);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(319, 39);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Condicionales";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Cyan;
+            this.button3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.DarkGray;
+            this.button3.Location = new System.Drawing.Point(29, 264);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(319, 39);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Ciclos";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Cyan;
+            this.button4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.DarkGray;
+            this.button4.Location = new System.Drawing.Point(29, 331);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(319, 39);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Vectores Y Mat y Lis";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pbx);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtnombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbx)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pbx;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Unidad1_API/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unidad1_API
+{
+
+    public partial class Form1 : Form
+    {
+        string Pais = "Colombia";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //string nombre = "Juan";
+            //int num_1 = 10;
+            //bool bl = true;
+            //char ch;
+            //decimal dec_1;
+            //float fl;
+            //double db = 50;
+            //long lg;
+            //object carro = new { llanta = "KENDA" };
+            //Pais = "Peru";
+
+            //lg = num_1;
+
+          
+            //var peso = carro;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string Nombre = txtnombre.Text;
+            int num1 , num2 , num3;
+            double dec1;
+
+            ///  Conversiones 
+
+            //num1 = int.Parse(Nombre);
+            //num1 = Convert.ToInt32(Nombre);
+
+            //dec1 = double.Parse(Nombre);
+            //dec1 = Convert.ToDouble(Nombre);
+            num1 = 30;
+            num2 = 10;
+
+            // operaciones
+            num3= num1 + num2;
+
+            // Fun con String
+            var substr = Nombre.Substring(0, 4);
+            var mayus = Nombre.ToUpper();
+            var minus = Nombre.ToLower();
+            var quitar_esp = Nombre.Trim();
+            var tamaño = Nombre.Length;
+
+            // concatenar 
+            string resultado = string.Concat(substr," - ",mayus," - " , tamaño," - ", minus );
+            resultado = string.Format("Mi nombre es:{0} y en mayus es:{1}",Nombre,mayus);
+
+
+            var Resulta_sum = suma(10 , 10);
+            MessageBox.Show(Resulta_sum.ToString());
+           
+
+        }
+
+
+
+        private void limpiar()
+        {
+            txtnombre.Text = "";
+        }
+
+        private int suma ( int p1 , int p2 , int p3 = 0)
+        {
+            int resultado = p1 + p2;
+            return resultado;
+
+        }
+
+        //private double suma(double p1, double p2)
+        //{
+        //    double resultado = p1 + p2;
+        //    return resultado;
+
+        //}
+        private int suma(string p1, string p2 )
+        {
+            int resultado = int.Parse(p1) + int.Parse(p2);
+            return resultado;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int num = 0;
+            string Nombre = txtnombre.Text;
+
+            ///   ||  or   &&   and
+            if (Nombre == "Juan" || Nombre == "Carlos")
+            {
+                num = 1;
+            }
+            else if(Nombre == "Pedro" && num == 0)
+            {
+                num = 2;
+            }
+            else 
+            {
+                num = 3;
+            }
+
+            if (num > 0)
+            {
+            }
+            else if( num >= 0)
+            {             
+            }
+
+
+            //// SWICHE
+            ///
+
+            string mes;
+            string txtMes = txtnombre.Text;
+
+            switch (txtMes)
+            {
+
+                case "1":
+                    mes = "Enero";
+                    break;
+
+                case "2":
+                    mes = "Febrero";
+                    break ;
+
+                default:
+                    mes = "No disponible";
+                    break;
+            }
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            int cl = int.Parse(txtnombre.Text);
+
+            //while(cl >= 0)
+            //{
+            //    MessageBox.Show(cl.ToString());
+            //    cl--;                
+            //}
+
+
+            //do
+            //{
+            //    MessageBox.Show("Cedula");
+
+            //}
+            //while (cl >= 18);
+
+
+            int total = 0;
+            for (int i = 0; i <= cl; i++)
+            {
+                if ((i % 2) == 0 )
+                {
+                    total++;
+                }
+            }
+
+            MessageBox.Show(String.Concat("Total de numero pares: " , total.ToString()));
+
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int[] Num = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+
+            Num[0] = 1;
+            Num[1] = 5;
+            Num[2] = 6;
+
+            //for (int i = 0; i < Num.Length; i++)
+            //{
+            //    MessageBox.Show(Num[i].ToString());
+
+            //}
+
+            //Num = new int[20];
+
+            //Array.Resize(ref Num, 20);   /// aumentando el tamaño del array
+
+            /// matrices 
+            /// 
+
+            int[,] mt;
+            mt = new int[2, 2] { { 1, 2 }, { 3, 4 } };
+
+
+
+            /// Listas
+            /// 
+
+            //List<string> Nombres = new List<string>() { "Juan", "Carlos" };
+            //Nombres.Add("David");
+            //Nombres.Add("Ana");
+
+
+            //Nombres.Remove("Carlos");
+            //Nombres.RemoveAt(0);
+
+
+            //string item = Nombres[0];
+
+            //for (int i = 0; i < Nombres.Count; i++)
+            //{
+            //    MessageBox.Show(Nombres[i]);
+            //}
+
+            //foreach (string nm in Nombres)
+            //{
+            //    MessageBox.Show(item);
+            //}
+
+
+
+            ///Diccionario
+            ///
+
+            Dictionary<int, string> User = new Dictionary<int, string>();
+            User.Add(1, "Juan");
+            User.Add(2, "Ana");
+            User.Add(3, "Jose");
+
+            User[1] = "Carlos";
+
+            User.Remove(1);
+            User.Clear();
+            var usuario = User[2];
+
+
+            foreach (var usr in User)
+            {
+                MessageBox.Show(usr.Value);
+            }
+
+
+
+
+    }
+    }
+}
