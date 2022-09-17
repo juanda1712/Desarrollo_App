f388c668db2bf370637e700e1d91bd3059081bb4
JUAN DAVID........ LOPEZ <juanda-1712@hotmail.com>
2022-09-17 16:12:54 -0500

    Clase API 01

---
+++ API_Unidad_2/Controllers/PruebasController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Unidad_2.Controllers
+{
+    [Route("test")]
+    [ApiController]
+    public class PruebasController : ControllerBase
+    {
+        public static List<Pruebas> items = new List<Pruebas>()
+        {
+            new Pruebas()
+            {
+                Id = 1,
+                Name = "Prueba 1"
+            },
+            new Pruebas()
+            {
+                Id=2,
+                Name = "Pruebas 2"
+            }
+
+        };
+
+
+        [HttpGet("Listar")]
+        public List<Pruebas> Consultar()
+        {
+            return items;
+        }
+
+
+        [HttpGet]
+        public Pruebas item(int id)
+        {
+            return items[0];
+        }
+
+        [HttpGet("Detalle")]
+        public dynamic Consultar_id(int id)
+        {
+            var det = items.Where(x => x.Id == id).ToList();
+            if (det.Count > 0 )
+            {
+                return det;
+            }
+            else
+            {
+                return new
+                {
+                    code= "Api001" ,
+                    message = "No existe registos"
+
+                };
+            }
+
+
+           
+        }
+
+
+
+    }
+}
---
+++ API_Unidad_2/Pruebas.cs
@@ -0,0 +1,10 @@
+﻿namespace API_Unidad_2
+{
+    public class Pruebas
+    {
+		public int Id { get; set; }
+        public string? Name { get; set; }
+
+
+    }
+}
